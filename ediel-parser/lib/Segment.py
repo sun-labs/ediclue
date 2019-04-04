92e56df169f3370451c3bf3741eb4763c31066c4
Victor Ingman <viregistermail@gmail.com>
2019-04-04 15:31:52 +0200

    structure for generic parser of segment definitions

--- ediel-parser/lib/Segment.py
+++ ediel-parser/lib/Segment.py
@@ -1,4 +1,16 @@
 class Segment():
+
+    def __init__(self, id = None, *, length=(None, None), min=None, max=None, mandatory=False):
+        self.id = id
+        self.length = length
+        self.min = min
+        self.max = max
+        self.mandatory = mandatory
+
+    def structure(self, children):
+        self.children = children
+        return self
+
     def __str__(self):
         keys = filter(lambda k: "__" not in k, dir(self))
         values = map(lambda k: "{}: {}".format(k, getattr(self, k)), keys)
---
+++ ediel-parser/lib/Segments/segmentDefinitions.py
@@ -0,0 +1,13 @@
+from lib.Segment import Segment
+
+definitions = {
+    "SEQ": Segment("SEQ", length=(0,3)).structure([
+        Segment("status_indicator_coded"), 
+        Segment("sequence_information", min=0, max=1).structure([
+            Segment("sequence_number", mandatory=True, length=(0,6)),
+            Segment("sequence_number_source", length=(0,3)),
+            Segment("code_list_qualifier", length=(0,3)),
+            Segment("code_list_responsible_agency", length=(0,3))
+        ])
+    ])
+}
