3921ec34587c51782a8f9f264bbd09aed44f5854
Victor Ingman <viregistermail@gmail.com>
2019-04-04 13:57:28 +0200

    added UNH segment

--- ediel-parser/lib/Segments/LOC.py
+++ ediel-parser/lib/Segments/LOC.py
@@ -4,16 +4,10 @@ https://www.truugo.com/edifact/d96a/loc/
 """
 class LOC(Segment):
     def __init__(self, segments):
-        print("hello from LOC")
         for i in range(len(segments)):
             d = segments[i]
-            if i == 0:
-                self.location_qualifier = d
-            elif i == 1:
-                self.location_identifier = d
-            elif i == 2:
-                self.related_location_one_identification = d
-            elif i == 3:
-                self.related_location_two_identification = d
-            elif i == 4:
-                self.relation_coded = d
+            if i == 0: self.location_qualifier = d
+            elif i == 1: self.location_identifier = d
+            elif i == 2: self.related_location_one_identification = d
+            elif i == 3: self.related_location_two_identification = d
+            elif i == 4: self.relation_coded = d
