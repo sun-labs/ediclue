5f6a18ac62cdb90b647ca6986c2a0e2f8956fe12
Victor Ingman <viregistermail@gmail.com>
2019-04-06 13:50:23 +0200

    added generator class

---
+++ ediel-parser/lib/EdielGenerator.py
@@ -0,0 +1,9 @@
+from lib.Message import Message
+from lib.Segment import Segment
+from lib.messageDefinitions import definitions
+
+class Generator():
+    
+    @staticmethod
+    def getAperakFor(segments: [Segment]) -> dict:
+        print("hello world")
