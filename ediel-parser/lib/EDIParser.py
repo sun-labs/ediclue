e9d952886bc4e1604328814712aac01cd840e7c9
Victor Ingman <viregistermail@gmail.com>
2019-04-07 16:10:51 +0200

    refactored ediparser class

--- ediel-parser/lib/EDIParser.py
+++ ediel-parser/lib/EDIParser.py
@@ -1,16 +1,17 @@
-from pydifact.message import Message
-from pydifact.segments import Segment as PySegment
-from lib.Segment import Segment
+from pydifact.message import Message as PMessage
+from pydifact.segments import Segment as PSegment
+
+from lib.UNMessage import UNMessage
+from lib.UNSegment import UNSegment
 
 class EDIParser():
     def __init__(self, payload: str):
         self.payload = payload
-        self.segments = Message.from_str(payload)
-        print(self.segments, self.payload)
+        self.message = PMessage.from_str(payload)
 
     @staticmethod
     def toEdifact(segments: list):
-        message = Message()
+        message = PMessage()
         for s in segments:
             if s is not None and len(s) > 0:
                 tag = s.pop(0)
