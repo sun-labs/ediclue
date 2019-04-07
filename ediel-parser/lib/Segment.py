ac840e8407021204400cef728827f3ee6efe3b1c
Victor Ingman <viregistermail@gmail.com>
2019-04-07 15:34:42 +0200

    refactoring list and dict functions to become class methods

--- ediel-parser/lib/Segment.py
+++ ediel-parser/lib/Segment.py
@@ -70,70 +70,38 @@ class Segment():
 
     """
     Convert dict edifact segment to
-    list form (for pydifact)
+    list form
     """
-    @staticmethod
-    def toList(segments):
-        if segments is None: return None
+    def toList(self):
+        return self._toList(segment=self)
+
+    def _toList(self, segment):
         result = []
         for k in segments.keys():
             segment = segments.get(k)
-            if type(segment) is not dict:
+            if type(segment) is not dict: # base case
                 result.append(segment)
-            else:
-                rec_result = Segment.toList(segment)
-                if rec_result is not None:
-                    result.append(rec_result)
+            else: # recursion
+                rec = self._toList(segment)
+                result.append(rec)
         return result
     
-
+    """
+    Create dictionary of segments with explanatory keys
+    """
     def toDict(self):
         return self._toDict(segment=self)
 
     def _toDict(self, segment):
-        parsed = {}
+        result = {}
         children = segment.children
         if len(children) > 0: # recursion
             for i in range(0, len(children)):
                 cur = children[i]
-                parsed[cur.id] = self._toDict(segment=cur)
+                result[cur.id] = self._toDict(segment=cur)
         else: # base case
-            parsed = None
-        return parsed
-
-    """
-    Validate and parse EDIFACT segment
-    @return the parsed segment
-    @error ValueError
-    """
-    def toDictOld(self, segment=None, children=None, tag=None):
-        children = self.children if children is None else children
-        n_children = len(children) if children is not None else 0
-        n_segments = len(segment) if segment is not None else 0
-        parsed = {}
-        if tag is not None:
-            parsed['tag'] = tag
-
-        if type(segment) is not list:
-            if self.id is None:
-                return segment
-            else:
-                parsed = segment
-        else:
-            for i in range(0, n_children):
-                segment_def = children[i]
-                is_mandatory = segment_def.mandatory
-                out_of_bounds = (i >= n_segments) and segment is not None
-                if out_of_bounds and is_mandatory:
-                    parsed[segment_def.id] = {
-                        'type': 'error',
-                        'message': 'mandatory field [{}] not supplied'.format(segment_def.id),
-                        'structure': segment_def
-                    }
-                if not out_of_bounds:
-                    value = segment[i] if segment is not None else None
-                    parsed[segment_def.id] = self.toDictOld(value, segment_def.children)
-        return parsed
+            result = segment.value
+        return result
 
 Group = Segment.create_group
 From = Segment.create_from
