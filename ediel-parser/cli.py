cf4efcf386b659828c78bf87264257f9739ea39a
Victor Ingman <viregistermail@gmail.com>
2019-04-07 16:11:01 +0200

    refactored cli

--- ediel-parser/cli.py
+++ ediel-parser/cli.py
@@ -1,24 +1,26 @@
 import argparse
 import sys
 import json
-from lib.EdielParser import EDIParser
-from lib.Generator import Generator
+from lib.EDIParser import EDIParser
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Parse EDIFACT data')
     parser.add_argument('--input', type=argparse.FileType('r'), default=sys.stdin)
     parser.add_argument('--output', type=argparse.FileType('w'), default=sys.stdout)
-    parser.add_argument('--from', choices=['edifact', 'json'], default='edifact')
+
+    parser.add_argument('--from', choices=['edifact'], default='edifact'),
     parser.add_argument('--to', choices=['json', 'raw', 'list'], default='json')
+
     parser.add_argument('--aperak', action='store_true')
 
     args = parser.parse_args()
 
-    input_data = args.input.read()
-    parser = EDIParser(input_data)
+    payload = args.input.read()
+    parser = EDIParser(payload)
 
     if args.aperak is True:
-        aperak = Generator.getAperakFor(parser.toDict())
+        aperak = parser.create_aperak()
         print(aperak)
-        exit(0)
+
+    
