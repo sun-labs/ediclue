4162677f65f248c32574f08a1642e65e7af32da8
Victor Ingman <viregistermail@gmail.com>
2019-04-07 15:35:16 +0200

    generator for aperak

--- ediel-parser/lib/Generator.py
+++ ediel-parser/lib/Generator.py
@@ -1,9 +1,13 @@
 from lib.Message import Message
 from lib.Segment import Segment
-from lib.messageDefinitions import definitions
+from lib.UNMessage import UNMessage
+
+import json
 
 class Generator():
     
     @staticmethod
-    def getAperakFor(segments: [Segment]) -> dict:
-        print("hello world")
+    def getAperakFor(message: Segment) -> Segment:
+        aperak = UNMessage('APERAK')
+        aperak['UNH']['message_reference_number'] = 'test'
+        print(json.dumps(aperak.toList()))
