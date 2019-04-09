cee8104e4f8191dc2151a45236a8fd18efda3faa
Victor Ingman <viregistermail@gmail.com>
2019-04-09 21:11:39 +0200

    added com segment

--- ediel-parser/lib/UNSegment.py
+++ ediel-parser/lib/UNSegment.py
@@ -5,6 +5,7 @@ from copy import deepcopy
 def UNSegment(segmentId, **args):
     segment = definitions.get(segmentId)
     if segment is None:
+        print("https://www.truugo.com/edifact/d96a/{}".format(segmentId))
         return Segment(tag=segmentId) # placeholder segment
     else:
         segment = deepcopy(segment)
