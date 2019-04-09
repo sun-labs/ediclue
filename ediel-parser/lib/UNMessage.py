18007ca1c3f15adc4402e8ba728dde0eba7354b1
Victor Ingman <viregistermail@gmail.com>
2019-04-09 13:06:07 +0200

    fixed reference errors with deep copies of objects

--- ediel-parser/lib/UNMessage.py
+++ ediel-parser/lib/UNMessage.py
@@ -1,8 +1,11 @@
 from lib.messageDefinitions import definitions
 from lib.Segment import Segment
 
+from copy import deepcopy
+
 def UNMessage(messageId, **args):
     message = definitions.get(messageId)
+    message = deepcopy(message)
     if message is None:
         print("https://www.truugo.com/edifact/d96a/{}".format(segmentId))
         return Segment(messageId) # placeholder segment
--- ediel-parser/lib/UNSegment.py
+++ ediel-parser/lib/UNSegment.py
@@ -1,8 +1,10 @@
 from lib.segmentDefinitions import definitions
 from lib.Segment import Segment
+from copy import deepcopy
 
 def UNSegment(segmentId, **args):
     segment = definitions.get(segmentId)
+    segment = deepcopy(segment)
     if segment is None:
         print("https://www.truugo.com/edifact/d96a/{}".format(segmentId))
         return Segment(tag=segmentId) # placeholder segment
