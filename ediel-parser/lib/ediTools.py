9a96e7a94d9d40c427aac28dd3929ee3790051f7
Victor Ingman <viregistermail@gmail.com>
2019-04-12 13:42:26 +0200

    fixed rstrip

--- ediel-parser/lib/ediTools.py
+++ ediel-parser/lib/ediTools.py
@@ -17,23 +17,28 @@ def rstrip(segments: [Segment]):
     return new_segments
 
 def _rstrip(segment: Segment):
+    new_segment = Segment.create_from(segment)
+    new_children = []
     decrement_val = 1
     end_index = len(segment)
     for i, seg in enumerate(reversed(segment)):
         if len(seg) > 0:
             stripped = _rstrip(seg)
             # print(i, 'len', len(seg), seg.id, segment.children[i].id, stripped.id)
-            segment.children[i] = stripped
+            # segment.children[i] = stripped
+            new_children.append(stripped)
             if len(stripped) == 0:
                 end_index -= decrement_val
             else:
                 decrement_val = 0 # stop decrementing as there's a value
         else:
+            new_children.append(seg)
             if seg.value is None:
                 end_index -= decrement_val
                 continue
             decrement_val = 0 # stop decrementing, but keep on cleaning rest
     # print(list(map(lambda x: ('old', x.id, x.value), segment.children)))
-    segment.children = segment.children[:end_index]
+    new_children = list(reversed(new_children)) # reverse back
+    new_segment.children = new_children[:end_index]
     # print(list(map(lambda x: (x.id, x.value), segment.children)))
-    return segment
+    return new_segment
