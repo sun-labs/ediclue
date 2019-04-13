7672e4f373a4c0a84ce23b3a5c2653fc4d454c11
Victor Ingman <viregistermail@gmail.com>
2019-04-13 19:23:53 +0200

    mail creation from edi message

--- ediel-parser/lib/cli/com.py
+++ ediel-parser/lib/cli/com.py
@@ -1,9 +1,11 @@
 import os
 from lib.EDICommunicator import EDICommunicator
+from lib.EDIParser import EDIParser
 
 def set_args(subparsers):
     parser = subparsers.add_parser('com', description='communication between EDI systems')
-    parser.add_argument('action', choices=['aperak', 'download'])
+    parser.add_argument('--send-to')
+    parser.add_argument('--from', dest='from_type', choices=['edi'], default='edi', help='The input content type'),
     parser.add_argument('--username', default=os.environ.get('SL_COM_USERNAME'))
     parser.add_argument('--password', default=os.environ.get('SL_COM_PASSWORD'))
     parser.add_argument('--server', default=os.environ.get('SL_COM_SERVER'))
@@ -14,8 +16,23 @@ def run(args):
     # dependencies on other arguments
     args.outgoing_server = args.server if args.outgoing_server is None else args.outgoing_server
     args.incoming_server = args.server if args.incoming_server is None else args.incoming_server
+    args.send_to = args.username if args.send_to is None else args.send_to
 
-    communicator = EDICommunicator()
-    communicator.server = args.server
-    communicator.username = args.username
-    communicator.password = args.password
+    payload = args.input.read()
+
+    parser = EDIParser(payload, format=args.from_type)
+    subject = parser['UNB'].toEdi()
+    print(subject)
+
+    com = EDICommunicator()
+    com.server = args.server
+    com.username = args.username
+    com.password = args.password
+
+    mail = com.create_edi_mail(
+        send_from='ediel@data.sunlabs.se',
+        send_to=args.send_to,
+        subject='UNB Segment',
+        file_content=payload
+    )
+    print(mail)
