2189323bdebd50855f130c14d70a2c7d3efc1a96
Victor Ingman <viregistermail@gmail.com>
2019-04-14 00:48:21 +0200

    parse multiple files at the same time

--- ediel-parser/lib/cli/parse.py
+++ ediel-parser/lib/cli/parse.py
@@ -2,16 +2,18 @@ import argparse
 import json
 import os
 from lib.EDIParser import EDIParser
+import lib.cli.tools as tools
 
 def set_args(subparsers):
     parser = subparsers.add_parser('parse', description='parsing of edi to supported formats and generation of messages')
-    parser.add_argument('--from', dest='from_type', choices=['edi', 'json'], default='edi'),
-    parser.add_argument('--to', dest='to_type', choices=['json', 'raw', 'json-arr', 'edi'], default='json')
+    parser.add_argument('--from', dest='from_type', choices=['edi', 'json', 'mail'], default='edi'),
+    parser.add_argument('--to', dest='to_type', choices=['json', 'raw', 'json-arr', 'edi', 'mail'], default='json')
     parser.add_argument('--aperak', action='store_true')
+    parser.add_argument('--input-dir')
+    parser.add_argument('--output-dir')
 
-def run(args):
-    payload = args.input.read()
-    parser = EDIParser(payload, format=args.from_type)
+def handle_parse(content, args):
+    parser = EDIParser(content, format=args.from_type)
 
     work_result = None
     if args.aperak is True:
@@ -28,4 +30,27 @@ def run(args):
     elif to_type == 'raw':
         result = payload
 
-    print(result)
+    return result
+
+def run(args):
+
+    if args.input_dir is not None:
+        full_paths = tools.get_files(args.input_dir)
+        for path in full_paths:
+            filename = '{}.{}'.format(os.path.basename(path), args.to_type)
+            fh = open(path, 'r')
+            content = fh.read()
+            fh.close()
+            result = handle_parse(content, args)
+            if args.output_dir is None:
+                print(result)
+            else:
+                path = os.path.join(args.output_dir, filename)
+                fh = open(path, 'w')
+                fh.write(result)
+                fh.close()
+        return args.output_dir
+    else:
+        payload = args.input.read()
+        result = handle_parse(payload, args)
+        print(result)
---
+++ ediel-parser/lib/cli/tools.py
@@ -0,0 +1,11 @@
+import os
+
+def get_files(path, blacklist=True):
+    filenames = os.listdir(path)
+    if blacklist is True:
+        filenames = filter(lambda f: f.lower() not in ['.ds_store'], filenames)
+    full_paths = map(lambda f: os.path.join(path, f), filenames)
+    files = filter(os.path.isfile, full_paths)
+    return files
+    # cleaned = map(lambda x: x.replace('.mail', ''), filenames)
+    # return list(filter(lambda f: f.isdigit(), cleaned))
