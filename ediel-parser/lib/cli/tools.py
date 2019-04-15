9fb9f8f876dd4ee174c1a05253eaa8fd2d243414
Victor Ingman <viregistermail@gmail.com>
2019-04-15 16:55:08 +0200

    new tools for cli and files

--- ediel-parser/lib/cli/tools.py
+++ ediel-parser/lib/cli/tools.py
@@ -9,3 +9,29 @@ def get_files(path, blacklist=True):
     return list(files)
     # cleaned = map(lambda x: x.replace('.mail', ''), filenames)
     # return list(filter(lambda f: f.isdigit(), cleaned))
+
+def filter_blacklist(file):
+    blacklist = ['.DS_Store']
+    return file not in blacklist
+
+def map_with_path(path):
+    return lambda f: os.path.join(path, f)
+
+def extract_filename(full_path):
+    return full_path.split('/')[-1]
+
+def get_files(path, cb=None, *, mode='r'):
+    filenames = os.listdir(path)
+    filenames = list(filter(filter_blacklist, filenames))
+    full_paths = list(map(map_with_path(path), filenames))
+    full_paths = list(filter(os.path.isfile, full_paths))
+    filenames = list(map(extract_filename, full_paths))
+    return filenames, full_paths
+
+# map files and open with file handler
+def map_files(full_paths: [str], cb=None, *, mode='r'):
+    if cb is not None:
+        for path in full_paths:
+            fh = open(path, mode)
+            cb(fh, path, extract_filename(path))
+            fh.close()
