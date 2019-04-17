6ebafd8c340cc74886f9bfead77ff5fb21ce63f7
Victor Ingman <viregistermail@gmail.com>
2019-04-17 19:42:43 +0200

    removed shebang, added python interpreter to config

--- bin/send-aperak-utilts.sh
+++ bin/send-aperak-utilts.sh
@@ -5,14 +5,14 @@ FMAILS="$TMP_FOLDER/mails"
 FAPERAK="$TMP_FOLDER/mails-aperak"
 IMAP_SEARCH_QUERY="OR (NOT ANSWERED SUBJECT UTILTS) (SUBJECT UTILTS FLAGGED)"
 
-source ../config/config.sh && \
+. ../config/config.sh && \
 mkdir -p "$FMAILS" && \
 mkdir -p "$FAPERAK" && \
 cd ../ediel-parser/ && \
-python cli.py com --imap-search-query "$IMAP_SEARCH_QUERY" --output-dir "$FMAILS" && \
-python cli.py parse --from mail --to mail --aperak --input-dir "$FMAILS" --output-dir "$FAPERAK" && \
-python cli.py com --input-dir /tmp/mails-aperak --imap-store-query \"+FLAGS\" "\\Seen \\Answered" --send | \
-python cli.py com --imap-store-query \"-FLAGS\" "\\Flagged"
+"$SL_PYTHON_EXEC" cli.py com --imap-search-query "$IMAP_SEARCH_QUERY" --output-dir "$FMAILS" && \
+"$SL_PYTHON_EXEC" cli.py parse --from mail --to mail --aperak --input-dir "$FMAILS" --output-dir "$FAPERAK" && \
+"$SL_PYTHON_EXEC" cli.py com --input-dir "$FAPERAK" --imap-store-query \"+FLAGS\" "\\Seen \\Answered" --send | \
+"$SL_PYTHON_EXEC" cli.py com --imap-store-query \"-FLAGS\" "\\Flagged"
 
-rm /tmp/mails/* 2> /dev/null
-rm /tmp/mails-aperak/* 2> /dev/null
+rm "$FMAILS/*" 2> /dev/null
+rm "$FAPERAK/*" 2> /dev/null
--- config/config.sample.sh
+++ config/config.sample.sh
@@ -3,3 +3,5 @@ export SL_COM_PASSWORD=""
 export SL_COM_SERVER=""
 export SL_COM_OUTGOING_SERVER=""
 export SL_COM_INCOMING_SERVER=""
+
+export SL_PYTHON_EXEC="/usr/bin/python3"
