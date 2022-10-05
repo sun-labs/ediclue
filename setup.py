392aa16309b68e722b4fb85c417eec070985f68d
svenpettersson <sven@hemma.se>
2022-10-05 10:06:13 +0200

    - updating version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ediclue',
-    version="0.0.7",
+    version="0.0.8",
     description='An open source parser for EDIEL data',
     url='https://github.com/sun-labs/ediclue',
     author='Sun Labs Nordic AB',
